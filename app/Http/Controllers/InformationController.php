0f7a28d7a6d4db24d07305dafb4ec867683d807a
Andrew <blader_100@mail.ru>
2020-09-28 14:55:03 +0300

    task 3

---
+++ app/Http/Controllers/InformationController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class InformationController extends Controller
+{
+    public function test(){
+
+
+        return view('home.test');
+    }
+
+    public function result()
+    {
+
+        $ch = curl_init('http://task/apiTest');
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        $html = curl_exec($ch);
+
+        curl_close($ch);
+
+        $arr = json_decode($html);
+
+        $fromUrl = $arr->fromUrl;
+
+        // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, $fromUrl);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $output = curl_exec($ch);
+
+        curl_close($ch);
+
+        $curl  = curl_init($fromUrl);
+
+        curl_setopt($curl ,CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($curl, CURLOPT_HEADER, false);
+
+        $resultFromUrl = curl_exec($curl);
+
+
+        $word = $arr->word;
+
+        $count = substr_count($resultFromUrl,$word);
+
+
+        $data = ["count" => $count];
+
+        $data_string = json_encode ($data, JSON_UNESCAPED_UNICODE);
+
+        $urlTo = $arr->toUrl;
+
+        $curl = curl_init($urlTo);
+
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+        // Принимаем в виде массива. (false - в виде объекта)
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string))
+        );
+
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+
+    }
+
+
+    public function posTtestPost(Request $request){
+
+        Log::info("Обработка формы");
+        Log::info($request->count);
+
+    }
+
+
+    public function apiTest(){
+
+
+        $a = array("fromUrl"=> 'http://task/test', "toUrl"=> "http://task/test1", "word"=>"во");
+
+        return json_encode($a, JSON_UNESCAPED_UNICODE);
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/test', 'InformationController@test')->name('test');
+Route::post('/test1', 'InformationController@posTtestPost')->name('postPost');
+Route::get('/apiTest', 'InformationController@apiTest')->name('apiTest');
+Route::get('/result', 'InformationController@result')->name('result');
+
